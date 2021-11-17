37801aa1d929d0661f0cc2df53abb665387a0c62
mokhtaria benaouali <benaoualimokhtaria@hotmail.com>
2021-11-17 17:13:58 +0100

    Javascript code

--- javascript.js
+++ javascript.js
@@ -1,20 +1,21 @@
-const citations = ["mimi", 
-                   "jqjhfsjhqjk",
-                   "hhjfshjqshjk",
+const citations = ["If you want something done right, do it yourself.",
+                   "Get up, stand up! Don't give up the fight!",
                    "usdyquui"];
-const autors = ["mimi", 
-                "noor", 
-                "amenay", 
-                "ceckkouka"];
-const emoji = ['128540', '1F61C', '1F61C'];
+const autors = ["Charles-Guillaume Étienne", 
+                "Bob Marley", 
+                "amenay"];
+const emojis = [0x1F5FA, 0x23F0, 0x1F579];
+      //['\u{128540}','\u{1F984}', '\u{1F61C}','\u{1F61C}'];
 let valueal = null;
 
 
 
 document.getElementById("generate").addEventListener("click",function(){
     valueal = Math.floor(Math.random() * citations.length);
+     document.getElementById('emoj').innerHTML = String.fromCodePoint(emojis[valueal]);
     document.getElementById('blockquote').innerHTML = citations[valueal];
     document.getElementById('aut').innerHTML = autors[valueal];
+   
     
 });
 
